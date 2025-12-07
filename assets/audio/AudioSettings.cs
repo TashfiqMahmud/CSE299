7adc81e52122e9c2bd51a50aab17d0e37a66fe08
tiara-irene <tiarairene000@gmail.com>
2025-12-08 00:25:12 +0600

    Update AudioSettings.cs

--- assets/audio/AudioSettings.cs
+++ assets/audio/AudioSettings.cs
@@ -3,34 +3,34 @@ using UnityEngine.UI;
 
 public class AudioSettings : MonoBehaviour
 {
-    public Slider audioSlider; // Music Slider
-    public Slider soundSlider; // SFX Slider
+    public Slider audioSlider; 
+    public Slider soundSlider; 
 
     void Start()
     {
-        // Load saved values (default = 1)
+       
         float savedMusic = PlayerPrefs.GetFloat("MusicVolume", 1f);
         float savedSFX = PlayerPrefs.GetFloat("SFXVolume", 1f);
 
-        // Set slider positions
+       
         audioSlider.value = savedMusic;
         soundSlider.value = savedSFX;
 
-        // Apply the saved volumes
+       
         SetMusicVolume(savedMusic);
         SetSFXVolume(savedSFX);
 
-        // Add listeners
+     
         audioSlider.onValueChanged.AddListener(SetMusicVolume);
         soundSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
     void SetMusicVolume(float value)
     {
-        // Save the value
+       
         PlayerPrefs.SetFloat("MusicVolume", value);
 
-        // Apply to all music sounds
+       
         foreach (var music in AudioManager.instance.musicSounds)
         {
             music.source.volume = value;
@@ -39,13 +39,14 @@ public class AudioSettings : MonoBehaviour
 
     void SetSFXVolume(float value)
     {
-        // Save the value
+      
         PlayerPrefs.SetFloat("SFXVolume", value);
 
-        // Apply to all SFX sounds
+
         foreach (var sfx in AudioManager.instance.sfxSounds)
         {
             sfx.source.volume = value;
         }
     }
 }
+
