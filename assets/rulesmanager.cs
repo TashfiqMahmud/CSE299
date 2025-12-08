2a48710ac3e14ab864639c38493700bedf26ae5a
tiara-irene <tiarairene000@gmail.com>
2025-12-08 10:06:27 +0600

    Update rulesmanager.cs

--- assets/rulesmanager.cs
+++ assets/rulesmanager.cs
@@ -2,25 +2,26 @@ using UnityEngine;
 
 public class rulesmanager : MonoBehaviour
 {
-    public GameObject rulesPanel; // Assign the RulesPanel here
+    public GameObject rulesPanel; 
 
     void Start()
     {
         if (rulesPanel != null)
-            rulesPanel.SetActive(false); // Hide panel at start
+            rulesPanel.SetActive(false); 
     }
 
-    // Called when "Rules" button is clicked
+    
     public void ShowRules()
     {
         if (rulesPanel != null)
             rulesPanel.SetActive(true);
     }
 
-    // Called when "Back" button is clicked
+   
     public void CloseRules()
     {
         if (rulesPanel != null)
             rulesPanel.SetActive(false);
     }
 }
+
