5bcda6743f1cf847438e64d6e437369b24222374
tiara-irene <tiarairene000@gmail.com>
2025-12-08 10:24:44 +0600

    Update winuimanagers.cs

--- assets/Script/winuimanagers.cs
+++ assets/Script/winuimanagers.cs
@@ -13,15 +13,13 @@ public class winuimanagers : MonoBehaviour
     {
         Time.timeScale = 1f;
 
-        // STOP CURRENT GAME MUSIC BEFORE SWITCHING SCENE
         if (AudioManager.instance != null)
             AudioManager.instance.Stop("GameSceneMusic");
 
-        // PLAY MAIN MENU MUSIC
         if (AudioManager.instance != null)
             AudioManager.instance.Play("MainMenuMusic");
 
-        // NOW LOAD SCENE
         SceneManager.LoadScene("MainMenu");
     }
 }
+
